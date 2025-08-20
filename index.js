435196172e9fa0a34f31c8ea85084cc76e6db156
NilxXD <nilxd.gd@gmail.com>
2025-08-20 14:58:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send("Bot is alive!"));
+app.listen(3000, () => console.log("Web server running on port 3000"));
+
 const { Client, GatewayIntentBits } = require('discord.js');
 
 const client = new Client({
